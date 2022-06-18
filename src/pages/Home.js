46b06734809248ea5b3e68a2df421c3512cb6899
william <williamkurniawan10@gmail.com>
2022-06-19 00:18:34 +0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import Navbar from "./Navbar";
+import "./Style.css";
 function home() {
   return (
-    <div>home</div>
-  )
+    <>
+      <div className="header">
+        Delivery slots are fully booked for 19 June. Latest pickup by 6pm.
+      </div>
+    </>
+  );
 }
 
-export default home
+export default home;
